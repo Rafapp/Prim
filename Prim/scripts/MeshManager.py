f7c21d699fbf32110b94b4f0b779b5c7d12a96fa
Rafapp <rpadiper@gmail.com>
2024-07-09 23:22:20 -0700

    Templated mesh manager

--- Prim/scripts/MeshManager.py
+++ Prim/scripts/MeshManager.py
@@ -1,11 +1,28 @@
-def cacheMeshes():
+# TODO Functionality:
+# - Check for corrupt data (e.g. file name changed) and prompt user
+
+#TODO Data:
+# - Current .prim file being used
+# - Absolute path to module folder
+
+def updateLibrary():
     pass
 
-def createMesh():
+# Renders a 3/4, lambertian, black and white preview of the primitive. Stores as .png image.
+def renderMeshPreview(name):
+    # Check if corresponding .obj file exists
+    # Render the mesh
     pass
 
-def savePrimitive():
+# Creates (instances) mesh to the current MAYA scene.
+def createMesh(name):
     pass
 
-def deletePrimitive():
+# Saves selected mesh in the scene to the .prim file, and updates library.
+def savePrimitive(name, data):
+    updateLibrary()
+    pass
+
+# Deletes mesh from .prim file, its .obj mesh, and its preview.
+def deletePrimitive(name):
     pass
